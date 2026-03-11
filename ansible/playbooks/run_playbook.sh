576b76021b13cb7de1e26df1c43e95da6ba1a795
Sergei Vavilov <sergei@zededa.com>
2026-03-11 15:01:37 +0100

    uploading playbooks

---
+++ ansible/playbooks/run_playbook.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+date
+
+#ansible-playbook list_programms.yaml
+#ansible-playbook list_configurations.yaml
+#ansible-playbook list_groups.yaml
+#ansible-playbook list_vm_usage.yaml
+#ansible-playbook list_get_next_token.yaml
+#ansible-playbook list_existing_vms.yaml
+#ansible-playbook create_vm_several.yaml
+#ansible-playbook create_vm_one.yaml
+#ansible-playbook create_vm_one4815.yaml
+ansible-playbook vms_update.yaml
+#ansible-playbook vm_regenerate_token.yaml
---
+++ ansible/readme.txt
@@ -0,0 +1,8 @@
+
+Fortinet's Ansible collection for managing fortiflex objects
+
+
+site: https://galaxy.ansible.com/fortinet/fortiflexvm
+repo: https://github.com/fortinet-ansible-dev/ansible-galaxy-fortiflexvm-collection
+docs: https://ansible-galaxy-fortiflexvm-docs.readthedocs.io/en/latest/index.html
+      https://docs.fortinet.com/document/flex-vm/23.2.0/administration-guide/887959/get-a-list-of-existing-vms-for-the-fortiflex-configuration
